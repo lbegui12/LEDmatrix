9dfe71c594ccb4206b12333ed0b179a85bc9fb38
lbegui12 <louis.beguin@live.fr>
2020-01-24 13:03:04 +0100

    Updated script to test com between HM10 bluetooth module and android device

--- test_hm10_bluetooth_module/test_hm10_bluetooth_module.ino
+++ test_hm10_bluetooth_module/test_hm10_bluetooth_module.ino
@@ -1,3 +1,71 @@
+//  SerialIn_SerialOut_HM-10_01
+//
+//  Uses hardware serial to talk to the host computer and AltSoftSerial for communication with the bluetooth module
+//
+//  What ever is entered in the serial monitor is sent to the connected device
+//  Anything received from the connected device is copied to the serial monitor
+//  Does not send line endings to the HM-10
+//
+//  Pins
+//  BT VCC to Arduino 5V out. 
+//  BT GND to GND
+//  Arduino D8 (SS RX) - BT TX no need voltage divider 
+//  Arduino D9 (SS TX) - BT RX through a voltage divider (5v to 3.3v)
+//
+ 
+#include <AltSoftSerial.h>
+AltSoftSerial BTserial; 
+// https://www.pjrc.com/teensy/td_libs_AltSoftSerial.html
+//     http://www.martyncurrey.com/hm-10-bluetooth-4ble-modules/ FOR MORE INFO
+ 
+char c=' ';
+boolean NL = true;
+ 
+void setup() 
+{
+    Serial.begin(9600);
+    Serial.print("Sketch:   ");   Serial.println(__FILE__);
+    Serial.print("Uploaded: ");   Serial.println(__DATE__);
+    Serial.println(" ");
+ 
+    BTserial.begin(9600);  
+    Serial.println("BTserial started at 9600");
+}
+ 
+void loop()
+{
+    // Read from the Bluetooth module and send to the Arduino Serial Monitor
+    if (BTserial.available())
+    {
+        c = BTserial.read();
+        Serial.write(c);
+    }
+ 
+ 
+    // Read from the Serial Monitor and send to the Bluetooth module
+    if (Serial.available())
+    {
+        c = Serial.read();
+ 
+        // do not send line end characters to the HM-10
+        if (c!=10 & c!=13 ) 
+        {  
+             BTserial.write(c);
+        }
+ 
+        // Echo the user input to the main window. 
+        // If there is a new line print the ">" character.
+        if (NL) { Serial.print("\r\n>");  NL = false; }
+        Serial.write(c);
+        if (c==10) { NL = true; }
+    }
+}
+
+
+
+/*
+ * 
+ * 
 #include <SoftwareSerial.h>
 SoftwareSerial BTSerial(2, 3); //RX|TX
 
@@ -17,3 +85,4 @@ void loop() {
   if (Serial.available())
     BTSerial.write(Serial.read());
 }
+*/
